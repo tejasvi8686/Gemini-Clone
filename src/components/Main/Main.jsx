7d25983afd3a0a02dfc9912c7d1ced0160d0036e
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-28 22:24:43 +0530

    UI Part done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,7 +12,41 @@ const Main = () => {
       <div className="main-container">
         <div className="greet">
           <p>
-            <span>Dev Hello</span>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can i help you today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>STell me about React js and React native</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Promt here" />
+            <div >
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps
           </p>
         </div>
       </div>
