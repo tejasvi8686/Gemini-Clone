276982db5db4bd4a9315a264a3f07610b8d35190
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-29 00:13:47 +0530

    gemni api integration

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -38,7 +38,7 @@ const Main = () => {
         <div className="main-bottom">
           <div className="search-box">
             <input type="text" placeholder="Enter a Promt here" />
-            <div >
+            <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
               <img src={assets.send_icon} alt="" />
---
+++ src/context/Context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+import runChat from "../config/Gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+	const [recentPrompt, setRecentPrompt] = useState("");
+	const [prevPrompts, setPrevPrompts] = useState([]);
+	const [showResults, setShowResults] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    await runChat(prompt)
+  };
+
+  onSent("What is react js");
+  const contextValue = {
+		prevPrompts,
+		setPrevPrompts,
+		onSent,
+		setRecentPrompt,
+		recentPrompt,
+		input,
+		setInput,
+		showResults,
+		loading,
+		resultData,
+		
+	};
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
