7568d0bbc330836074902790f242b71518b9d4a8
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-29 00:56:02 +0530

    add generative AI Functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,14 +4,8 @@ import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
 const Main = () => {
-  const {
-    onSent,
-    recentPrompt,
-    shoeResults,
-    loading,
-    setInput,
-    input,
-  } = useContext(Context)
+  const { onSent, recentPrompt, showResults, loading, resultData, setInput, input } =
+    useContext(Context);
   return (
     <div className="main">
       <div className="nav">
@@ -19,42 +13,58 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How can i help you today</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
+        {!showResults ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can i help you today</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
 
-            <img src={assets.bulb_icon} alt="" />
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>STell me about React js and React native</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+            </div>
           </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>STell me about React js and React native</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
             <input
-              onChange={(e) => setInput (e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
               type="text"
               placeholder="Enter a Promt here"
             />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img onClick={()=>onSent()}  src={assets.send_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
           <p className="bottom-info">
