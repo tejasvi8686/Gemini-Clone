928ddfc06b3ffc7ebbfff95a24eee2b81c56e6ef
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-28 18:54:06 +0530

    project setup and sidebar

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemni</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Dev Hello</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
